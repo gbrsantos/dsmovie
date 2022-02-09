7b845197d141b2e47972ee7616fb92e57dc71bfb
Gabriel Santos <gabriel-santos00@hotmail.com>
2022-02-09 18:21:14 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -5,9 +5,6 @@ import Pagination from "../../components/Pagination";
 import { MoviePage } from "../../types/movie";
 import { BASE_URL } from "../../utils/requests";
 function Listing() {
-
-
-
   const [pageNumber, setPageNumber] = useState(0);
   const [page, setPage] = useState<MoviePage>({
     content: [],
@@ -22,23 +19,26 @@ function Listing() {
   });
   useEffect(() => {
     axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
-    .then(response => {
+      .then(response => {
 
-      const data = response.data as MoviePage;
-      setPage(data);
-    });
+        const data = response.data as MoviePage;
+        setPage(data);
+      });
   }, [pageNumber]);
 
+  const handlePageChange = (newPageNumber : number)=>{
+    setPageNumber(newPageNumber);
+  }
   return (
     <>
-      <Pagination />
+      <Pagination page={page} onChange={handlePageChange} />
       <div className="container">
         <div className="row">
           {page.content.map(movie => (
-            <div key = {movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard movie={movie} />
-          </div>
-          )            
+            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+              <MovieCard movie={movie} />
+            </div>
+          )
           )}
 
         </div>
